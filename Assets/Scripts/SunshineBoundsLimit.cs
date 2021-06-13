16af157f0aaf9c84093779d12515d1b6c8343dc9
walterlv <xblero@gmail.com>
2021-06-13 11:21:53 +0800

    Rigidbody

--- Assets/Scripts/SunshineBoundsLimit.cs
+++ Assets/Scripts/SunshineBoundsLimit.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class SunshineBoundsLimit : MonoBehaviour
@@ -7,7 +8,7 @@ public class SunshineBoundsLimit : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -16,20 +17,35 @@ public class SunshineBoundsLimit : MonoBehaviour
         LimitPosition();
     }
 
-    public float MinX;
-    public float MinY;
-    public float MinZ;
-    public float MaxX;
-    public float MaxY;
-    public float MaxZ;
+    public float MinX = -1;
+    public float MinY = 0;
+    public float MinZ = -0.6f;
+    public float MaxX = 1.2f;
+    public float MaxY = 1.6f;
+    public float MaxZ = 0.5f;
 
     public void LimitPosition()
     {
         Transform trNeedLimit = transform;
         var rigidbody = GetComponent<Rigidbody>();
 
-        trNeedLimit.position = new Vector3(Mathf.Clamp(trNeedLimit.position.x, -1, 1.2f),
-                                           Mathf.Clamp(trNeedLimit.position.y, 0, 1.6f),
-                                           Mathf.Clamp(trNeedLimit.position.z,-0.6f,0.5f));
+        if(trNeedLimit.position.x<MinX || trNeedLimit.position.x > MaxX)
+        {
+            rigidbody.velocity = new Vector3(0, rigidbody.velocity.y, rigidbody.velocity.z);
+        }
+        if(trNeedLimit.position.y<MinY || trNeedLimit.position.y > MaxY)
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x,0, rigidbody.velocity.z);
+        }
+        if(trNeedLimit.position.z<MinZ || trNeedLimit.position.z > MaxZ)
+        {
+            rigidbody.velocity = new Vector3(rigidbody.velocity.x, rigidbody.velocity.y, 0);
+        }
+
+        rigidbody.rotation = new Quaternion();
+
+        trNeedLimit.position = new Vector3(Mathf.Clamp(trNeedLimit.position.x, MinX, MaxX),
+                                           Mathf.Clamp(trNeedLimit.position.y, MinY, MaxY),
+                                           Mathf.Clamp(trNeedLimit.position.z, MinZ, MaxZ));
     }
 }
